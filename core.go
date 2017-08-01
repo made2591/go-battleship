2554b093067de1df5b84bb1d7d030909eca34b15
Matteo Madeddu <matteo.madeddu@gmail.com>
2017-08-01 13:19:45 +0200

    Fix field and methods cases.

--- core.go
+++ core.go
@@ -8,27 +8,29 @@ import (
 )
 
 type Game struct {
-	sea_a Sea
-	sea_b Sea
-	moves []Coordinates
+	Sea_a Sea
+	Sea_b Sea
+	Moves_vs_a []Coordinates
+	Moves_vs_b []Coordinates
 }
 
 type Sea struct {
-	dimension int
-	grid      [][]int
-	ships     []Ship
-	moves     []Coordinates
+	Dimension int
+	Grid      [][]int
+	Ships     []Ship
+	Moves     []Coordinates
 }
 
 type Ship struct {
-	dimension int
-	positions []Coordinates
-	available []Coordinates
+	Dimension int
+	Positions []Coordinates
+	Available []Coordinates
 }
 
 type Coordinates struct {
-	abscissa int
-	ordinate int
+	Abscissa int
+	Ordinate int
+	Status   int
 }
 
 func random(min, max int) int {
@@ -46,7 +48,7 @@ func search(a int, b []int) bool {
 	return false
 }
 
-// PrepareSea return a Sea struct given dimension n
+// PrepareSea return a Sea struct given Dimension: n
 func PrepareSea(n int, s int) (sea Sea) {
 
 	ss := make([]Ship, s)
@@ -54,7 +56,7 @@ func PrepareSea(n int, s int) (sea Sea) {
 	for i := 0; i < s; i++ {
 
 		st := PrepareShip(i+1, n)
-		if !checkCollisions(st, ss) {
+		if !CheckCollisions(st, ss) {
 			ss = append(ss, PrepareShip(i+1, n))
 		} else {
 			i--
@@ -62,7 +64,7 @@ func PrepareSea(n int, s int) (sea Sea) {
 
 	}
 
-	sea = Sea{dimension: n, grid: make([][]int, n), ships: ss, moves: []Coordinates{}}
+	sea = Sea{Dimension: n, Grid: make([][]int, n), Ships: ss, Moves: []Coordinates{}}
 
 	return
 
@@ -80,8 +82,8 @@ func PrepareShip(n int, m int) (s Ship) {
 
 		x := random(1, m)
 		y := random(1, m)
-		p[0] = Coordinates{abscissa: x, ordinate: y}
-		a[0] = Coordinates{abscissa: x, ordinate: y}
+		p[0] = Coordinates{Abscissa: x, Ordinate: y}
+		a[0] = Coordinates{Abscissa: x, Ordinate: y}
 
 	} else {
 
@@ -92,28 +94,28 @@ func PrepareShip(n int, m int) (s Ship) {
 
 			if h {
 
-				p[t] = Coordinates{abscissa: x + t, ordinate: y}
-				a[t] = Coordinates{abscissa: x + t, ordinate: y}
+				p[t] = Coordinates{Abscissa: x + t, Ordinate: y}
+				a[t] = Coordinates{Abscissa: x + t, Ordinate: y}
 
 			} else {
 
-				p[t] = Coordinates{abscissa: y, ordinate: x + t}
-				a[t] = Coordinates{abscissa: y, ordinate: x + t}
+				p[t] = Coordinates{Abscissa: y, Ordinate: x + t}
+				a[t] = Coordinates{Abscissa: y, Ordinate: x + t}
 
 			}
 		}
 
 	}
 
-	s = Ship{dimension: n, positions: p, available: a}
+	s = Ship{Dimension: n, Positions: p, Available: a}
 	return
 
 }
 
-func checkCollisions(a Ship, b []Ship) bool {
+func CheckCollisions(a Ship, b []Ship) bool {
 
 	for _, sb := range b {
-		if checkCollision(a, sb) {
+		if CheckCollision(a, sb) {
 			return true
 		}
 	}
@@ -121,11 +123,11 @@ func checkCollisions(a Ship, b []Ship) bool {
 
 }
 
-func checkCollision(a Ship, b Ship) bool {
+func CheckCollision(a Ship, b Ship) bool {
 
-	for _, av := range a.positions {
-		for _, bv := range b.positions {
-			if av.abscissa == bv.abscissa && av.ordinate == bv.ordinate {
+	for _, av := range a.Positions {
+		for _, bv := range b.Positions {
+			if av.Abscissa == bv.Abscissa && av.Ordinate == bv.Ordinate {
 				return true
 			}
 		}
@@ -134,11 +136,11 @@ func checkCollision(a Ship, b Ship) bool {
 
 }
 
-func checkPosition(x int, y int, s Sea) bool {
+func CheckPosition(x int, y int, s Sea) bool {
 
-	for _, sv := range s.ships {
-		for _, cv := range sv.positions {
-			if x == cv.abscissa && y == cv.ordinate {
+	for _, sv := range s.Ships {
+		for _, cv := range sv.Positions {
+			if x == cv.Abscissa && y == cv.Ordinate {
 				return true
 			}
 		}
@@ -149,15 +151,15 @@ func checkPosition(x int, y int, s Sea) bool {
 
 func CoordinatesPrettyInfo(c Coordinates) (cs string) {
 
-	cs = "(" + strconv.Itoa(c.abscissa) + "; " + strconv.Itoa(c.ordinate) + ")"
+	cs = "(" + strconv.Itoa(c.Abscissa) + "; " + strconv.Itoa(c.Ordinate) + ")"
 	return
 
 }
 
 func ShipPrettyInfo(s Ship) (ss string) {
 
-	ss = "\tShip dimensions: " + strconv.Itoa(s.dimension) + "\n\t\t["
-	for _, pv := range s.positions {
+	ss = "\tShip dimensions: " + strconv.Itoa(s.Dimension) + "\n\t\t["
+	for _, pv := range s.Positions {
 		ss += CoordinatesPrettyInfo(pv) + " "
 	}
 	ss += "]"
@@ -167,9 +169,9 @@ func ShipPrettyInfo(s Ship) (ss string) {
 
 func SeaPrettyInfo(s Sea) (ss string) {
 
-	ss = "Sea dimensions: " + strconv.Itoa(s.dimension) + "\n"
-	for _, sv := range s.ships {
-		if sv.dimension != 0 {
+	ss = "Sea dimensions: " + strconv.Itoa(s.Dimension) + "\n"
+	for _, sv := range s.Ships {
+		if sv.Dimension != 0 {
 			ss += ShipPrettyInfo(sv) + "\n"
 		}
 	}
@@ -180,16 +182,16 @@ func SeaPrettyInfo(s Sea) (ss string) {
 func StringfySea(s Sea) (ss string) {
 
 	ss = "|"
-	for r := 0; r < s.dimension-1; r++ {
+	for r := 0; r < s.Dimension-1; r++ {
 		ss += "-----"
 	}
 	ss += "----|\n"
 
-	for r := 0; r < s.dimension; r++ {
+	for r := 0; r < s.Dimension; r++ {
 
 		ss += "|"
-		for c := 0; c < s.dimension; c++ {
-			if checkPosition(r+1, c+1, s) {
+		for c := 0; c < s.Dimension; c++ {
+			if CheckPosition(r+1, c+1, s) {
 				ss += " ** |"
 			} else {
 				ss += "    |"
@@ -197,7 +199,7 @@ func StringfySea(s Sea) (ss string) {
 		}
 		ss += "\n"
 		ss += "|"
-		for c := 0; c < s.dimension-1; c++ {
+		for c := 0; c < s.Dimension-1; c++ {
 			ss += "-----"
 		}
 		ss += "----|\n"
