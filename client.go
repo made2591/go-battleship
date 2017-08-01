1e699ac467158f0e278e8e50a469c20183aa4877
Matteo Madeddu <matteo.madeddu@gmail.com>
2017-08-01 22:30:28 +0200

    Add first route of test (game creation).

--- client.go
+++ client.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	core "github.com/made2591/go-battleship/core"
+	"net/http"
+	//"time"
+)
+
+func main() {
+
+	g := core.Game{}
+	fmt.Println(">>> request new game...")
+	res, _ := http.Get("http://localhost:8080/start")
+	json.NewDecoder(res.Body).Decode(&g)
+	fmt.Println(">>> Matteo")
+	fmt.Println(core.StringfySea(g.FirstPlayer.Sea))
+	fmt.Println(">>> HAL")
+	fmt.Println(core.StringfySea(g.SecondPlayer.Sea))
+
+}
--- server.go
+++ server.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	core "github.com/made2591/go-battleship/core"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// }
+
+func start(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(">>> new game generation...")
+	g := core.PrepareGame(10, 0, "Matteo", 5, 9999, "HAL", 5, 9999)
+	fmt.Println(">>> Matteo")
+	fmt.Println(core.StringfySea(g.FirstPlayer.Sea))
+	fmt.Println(">>> HAL")
+	fmt.Println(core.StringfySea(g.SecondPlayer.Sea))
+	json.NewEncoder(w).Encode(g)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/start", start)
+	//http.HandleFunc("/gunshot", gunshot)
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("cia")
 }
